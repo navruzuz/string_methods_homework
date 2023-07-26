b0dbd66ab3be0476a629a0c28b2fbd85eabaa0fb
navruzuz <hamrayevnavruzbek9906@gmail.com>
2023-07-26 11:25:30 +0500

    baajrildi

--- String01.py
+++ String01.py
@@ -1,10 +1,4 @@
 def main(s):
-    """
-    A str of several words is given. All letters are lowercase. Make sure that the first letter of each word is capitalized.
-    Args:
-        s: str
-    Returns:
-        str: answer
-    """
-    
-    return
+    s='python dasturlash tili'
+    return s.title()
+print(main('python dasturlash tili'))
